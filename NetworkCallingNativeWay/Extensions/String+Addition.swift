21269e3728dbe72023b5309eda30f6a3d55636e3
SHIVANSHU VERMA <41211881+sshivanshu992@users.noreply.github.com>
2024-09-05 11:02:17 +0530

    String extension updated

--- NetworkCallingNativeWay/Extensions/String+Addition.swift
+++ NetworkCallingNativeWay/Extensions/String+Addition.swift
@@ -8,5 +8,32 @@
 import Foundation
 
 extension String {
-    
+    /// Function to convert date format
+    func formattedDate(
+        output format: String = "MMM dd, yyyy, h:mm:ss a",
+        inputDateFormate: String = "yyyy-MM-dd'T'HH:mm:ss.SS"
+    ) -> String {
+        /// Define the input date formatter
+        let inputFormatter = DateFormatter.formatter(with: inputDateFormate)
+
+        /// Convert the input string to a Date object
+        guard let date = inputFormatter.date(from: self) else { return "" }
+
+        /// Define the output date formatter
+        let outputFormatter = DateFormatter.formatter(with: format)
+
+        /// Convert the Date object to the desired output format
+        let outputDate = outputFormatter.string(from: date)
+        return outputDate
+    }
+
+    func timeAgoString() -> String {
+        /// Define the input date formatter
+        let inputFormatter = DateFormatter.iso8601WithFractionalSecondsFormatter
+
+        /// Convert the input string to a Date object
+        guard let dateTime = inputFormatter.date(from: self) else { return "" }
+
+        return dateTime.timeAgoString(fallbackFormattedDate: self)
+    }
 }
